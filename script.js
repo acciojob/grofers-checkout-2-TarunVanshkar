bce2bfaa9847ad300ead17345c2184aae4e09e18
TarunVanshkar <117818351+TarunVanshkar@users.noreply.github.com>
2023-05-17 20:57:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your code here
+let table = document.querySelector("table");
+let priceList = document.querySelectorAll("[data-ns-test=prices]");
+// console.log(priceList)
+let totalPrice = 0;
+priceList.forEach((item) => {
+    totalPrice += Number(item.innerText);
+})
+// console.log(totalPrice)
+
+let lastRow = document.createElement("tr");
+let tableData = document.createElement("td");
+tableData.setAttribute("data-ns-test", "grandTotal");
+tableData.innerText = totalPrice;
+lastRow.appendChild(tableData);
+table.appendChild(lastRow);
